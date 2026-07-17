1d3c91fad75d868d849bf6ef7fabfd31ddbfca2e
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-07-17 08:43:21 +0530

    Implement sum of digits logic with while loop

---
+++ Loops/SumOfDigits.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+     public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        long n = sc.nextLong();
+
+        int sum = 0 ;
+
+        
+        while(n != 0){
+            sum += n%10 ;
+            n /= 10;
+        }
+
+        System.out.println("Sum: "+sum);
+
+
+        sc.close();
+     }
+}

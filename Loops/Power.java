bcbe1b431b76a4957b8d46bbf5720fed1d463d7a
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-07-17 10:17:21 +0530

    Add power calculation program in Java

---
+++ Loops/Power.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Power {
+    
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a: ");
+        int a = sc.nextInt();
+
+        System.out.println("Enter b: ");
+        int b = sc.nextInt();
+
+        int pow = 1;
+
+        for(int i = 1; i <= b; i++){
+             pow *= a;
+        }
+
+        System.out.println(a + " raised to the power " + b + " is " + pow);
+       
+
+        
+    
+    }
+    
+}

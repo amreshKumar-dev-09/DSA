dcec0ef1bb14e7b86fd265e8dd302bb104aa050f
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-07-17 10:37:12 +0530

    feat: add absolute value program using if-else

---
+++ IfElse/AbsoluteValue.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AbsoluteValue {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num < 0) {
+            num = -num;   // convert negative to positive
+        }
+
+        System.out.println("Absolute value is: " + num);
+
+        sc.close();
+    }
+}

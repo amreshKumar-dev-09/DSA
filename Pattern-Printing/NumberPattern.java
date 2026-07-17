0ee31331c54b46c1d575fe44a0c7921a17b5dad2
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-07-17 18:01:26 +0530

    feat(pattern): add number pattern using nested loops

---
+++ Pattern-Printing/NumberPattern.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class NumberPattern {
+      public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of rows: ");
+        int r = sc.nextInt();
+
+        System.out.println("Enter number of columns: ");
+        int c = sc.nextInt();
+      
+        
+        for(int i = 1; i <= r; i++){
+            for(int j = 1; j <= c; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+    
+}

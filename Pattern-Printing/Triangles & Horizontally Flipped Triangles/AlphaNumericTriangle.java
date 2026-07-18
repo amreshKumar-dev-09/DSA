0b8de5bea23af3fc3e9a909596680aae91d0b894
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-07-18 08:24:32 +0530

    feat: Add alphanumeric triangle pattern using nested loops

---
+++ Pattern-Printing/Triangles & Horizontally Flipped Triangles/AlphaNumericTriangle.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class AlphaNumericTriangle {
+     public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        if (rows <= 0) {
+            System.out.println("Please enter a positive number");
+        }
+
+        for(int i = 1; i <= rows; i++){
+            for(int j = 1; j <= i; j++){
+                if(i % 2 == 1)
+                    System.out.print((char)(j+64) + " ");
+            else
+                System.out.print(j + " ");
+                
+            }
+            System.out.println();
+            
+        }
+
+        sc.close();
+    
+    }
+    
+}

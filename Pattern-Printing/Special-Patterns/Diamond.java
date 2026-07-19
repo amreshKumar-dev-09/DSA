64935fcb4960fbd8eb22af0a4e6ced412acc26c8
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-07-19 16:20:07 +0530

    Built diamond pattern using variable-based approach (nsp/nst); fixed lower half iteration and documented star loop behavior

---
+++ Pattern-Printing/Special-Patterns/Diamond.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Diamond {
+     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int r = sc.nextInt();
+
+        sc.close();
+
+        int nsp = r - 1;
+        int nst = 1;
+
+        if (r <= 0) {
+            System.out.println("Please enter a positive number");
+            return;
+        }
+
+        for (int i = 1; i <= r; i++) {
+
+            // print spaces (decreasing)
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print("  ");
+            }
+
+            // print stars
+            for (int j = 1; j <= nst; j++) {
+                System.out.print("* ");
+            }
+            nsp -= 1;
+            nst += 2;
+            System.out.println();
+        }
+
+        nsp = 1;
+        nst = 2 * r - 3;
+
+         for (int i = 1; i <= r; i++) {
+
+            // print spaces (decreasing)
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print("  ");
+            }
+
+            // print stars
+            for (int j = 1; j <= nst; j++) {
+                System.out.print("* ");
+            }
+            nsp++;
+            nst -= 2;
+            System.out.println();
+        }
+
+
+
+
+
+    }
+}

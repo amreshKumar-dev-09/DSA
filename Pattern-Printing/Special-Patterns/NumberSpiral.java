123fdcff572b1f7e9845cf767d18b5d328887431
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-07-20 06:56:57 +0530

    feat: Add number spiral pattern using min logic

---
+++ Pattern-Printing/Special-Patterns/NumberSpiral.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class NumberSpiral {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int r = sc.nextInt();
+
+        sc.close();
+
+        if (r <= 0) {
+            System.out.println("Please enter a positive number");
+            return;
+        }
+
+        for (int i = 1; i <= 2 * r - 1; i++) {
+            for (int j = 1; j <= 2 * r - 1; j++) {
+                int a = (i <= r) ? i : 2 * r - i;
+                int b = (j <= r) ? j : 2 * r - j;
+                System.out.print(Math.min(a, b) + " ");
+            }
+            System.out.println();
+        }
+    }
+}

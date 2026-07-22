6bdf580a239d90c34f2c091161a9ca777e3c08e2
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-07-22 21:39:58 +0530

    feat: Fix inverted pyramid pattern spacing logic

---
+++ Pattern-Printing/Special-Patterns/InvertedPyramid.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class InvertedPyramid {
+    public static void main(String a[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of rows: ");
+        int r = sc.nextInt();
+
+        sc.close();
+
+
+        if (r <= 0) {
+            System.out.println("Please enter a positive number");
+            return;
+        }
+
+        for (int i = r; i >= 1; i--) {
+
+            // print spaces (decreasing)
+            for (int j = 1; j <= r-i; j++) {
+                System.out.print("  ");
+            }
+
+            // print stars
+            for (int j = 1; j <= 2*i-1; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+
+    }
+     
+        }
+    }
+

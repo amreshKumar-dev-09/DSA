7f8212a6f95287ec2f1ec11c5ea01f9c60d2f461
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-08-04 07:36:00 +0530

    Added optimized divisor calculation using √n approach

---
+++ Loops/PrintDivisors.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class PrintDivisors{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+         List<Integer> list = new ArrayList<>();
+
+        System.out.println("Enter a Digit: ");
+        int n = sc. nextInt();
+
+        for (int i = 1; i*i <= n; i++){
+
+            if (n % i == 0){
+             list.add(i);
+
+              if (i != n/i ){
+                 list.add(n/i);
+              }
+               
+            }
+
+        }
+        
+        Collections.sort(list);
+
+        System.out.println(list);
+
+        sc.close();
+
+
+    }
+}

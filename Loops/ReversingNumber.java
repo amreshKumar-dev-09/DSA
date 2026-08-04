5ae924daf7685c0953646d0ff3c02914a6604044
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-08-04 20:24:54 +0530

    feat: Add functionality to reverse a given number

--- Loops/ReversingNumber.java
+++ Loops/ReversingNumber.java
@@ -1,26 +1,33 @@
 import java.util.Scanner;
 
 public class ReversingNumber {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a number: ");
-        long n = sc.nextLong();
+        System.out.println("Enter a Digit: ");
+        int n = sc. nextInt();
 
-        long reverseNumber = 0;
+        int lastdigit = 0;
+        int reverse = 0;
 
         while(n != 0){
 
-        
-             reverseNumber *= 10;
-             reverseNumber += n%10;
-             
-             n /= 10;
+            lastdigit = n%10;
+
+            // Overflow check
+            if (reverse > Integer.MAX_VALUE / 10 || 
+                reverse < Integer.MIN_VALUE / 10) {
+                System.out.println("Overflow occurred. Cannot reverse safely.");
+                sc.close();
+                return;
+            }
+            
+            reverse = reverse*10 + lastdigit;
+            n = n/10;
         }
-
-        System.out.println(reverseNumber);
-
-
+        
     }
+
+
+        
 }

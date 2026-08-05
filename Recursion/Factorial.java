58d5a3783cd18def4cc1ed90ebee44b22397e38c
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-08-05 09:24:14 +0530

    feat: Add factorial program using recursion in Java

---
+++ Recursion/Factorial.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Operation{
+
+    int fact(int n){
+
+        if (n == 0){
+            return 1;
+        }
+
+        return n * fact(n - 1); 
+            
+    }
+}
+
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        Operation obj = new Operation();
+        System.out.println("Factorial: " + obj.fact(n));
+        
+    }
+    
+}

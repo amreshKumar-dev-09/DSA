9d675ed800f2acede154a9a5ede9f42f0bdde18c
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-08-26 08:26:03 +0530

    Add array frequency counting using HashMap

---
+++ Recursion/countArrayFreq.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class countArrayFreq{
+    public static void main(String a[]){
+
+        int arr[] = {1,2,1,2,3};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < arr.length; i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i]) + 1);
+            }
+            else{
+                map.put(arr[i],1);
+            }
+        }
+
+        System.out.println(map);
+    }
+}

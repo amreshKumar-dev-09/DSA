ece23c85ea5e53a161bbbb6948318b09dc63a05a
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-08-29 22:17:36 +0530

    feat: Added optimized factorial program using recursion in Java

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -1,31 +1,27 @@
 import java.util.Scanner;
 
+// Time and space complexity is O(n).
+
 class Operation{
-
     int fact(int n){
-
         if (n == 0){
             return 1;
         }
-
         return n * fact(n - 1); 
-            
     }
 }
-
-// Time and space complexity is O(n).
 
 public class Factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        
+     
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
 
         Operation obj = new Operation();
         System.out.println("Factorial: " + obj.fact(n));
+
+        sc.close();
         
-    }
-    
+    }   
 }

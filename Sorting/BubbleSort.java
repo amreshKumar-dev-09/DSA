c1c25cfacd4e416038e8a2f2bda2393888097c70
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-08-29 08:19:16 +0530

    feat: implement optimized Bubble Sort algorithm

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,22 +1,30 @@
+// Time Complexity = O(n^2) for worst and average case.
+// Time Complexity = O(n) for best case.
+// Space Complexity = O(1). 
+ 
  public class BubbleSort{
  public static void sort(int[] arr){
         int n = arr.length;
         for(int i = n - 1; i >= 1; i--){
+            int didswap = 0;
 
             for(int j = 0; j <= i - 1; j++){
 
                 if(arr[j] > arr[j + 1]){
-                     int temp = arr[j+1];
-                     arr[j+1] = arr[j];
-                     arr[j] = temp;
-
+                     int temp = arr[j];
+                     arr[j] = arr[j + 1];
+                     arr[j + 1] = temp;
+                     didswap = 1;
                 }
                 
             }
 
+            if(didswap == 0){
+                break;
+            }
         }
-
     }
+    
 
     public static void print(int[] arr){
          for (int num : arr) {

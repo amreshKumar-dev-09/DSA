7b11948e60c2498bee90c3a8c6c852def1d35522
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-08-30 09:03:02 +0530

    Added Quick Sort implementation

---
+++ Sorting/QuickSort.java
@@ -0,0 +1,56 @@
+public class QuickSort{
+
+    public static int partition(int[] arr, int low, int high){
+        int i = low;
+        int j = high;
+        int pivot = arr[low];
+
+        while(i < j){
+
+            while(arr[i] <= pivot && i <= high){
+                i++;
+            }
+
+            while(arr[j] > pivot && j >= low + 1){
+                j--;
+            }
+
+            if(i < j){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+
+        return j;
+    }
+
+    public static void sort(int[] arr, int low, int high){
+
+        if(low < high){
+            int p = partition(arr, low, high);
+
+            sort(arr, low, p - 1);
+            sort(arr, p + 1, high);
+
+        }
+    }
+
+    public static void main(String[] args){
+        int arr[] = {4,6,2,5,7,9,1,3};
+        int n = arr.length;
+
+        sort(arr, 0, n - 1);
+
+        for(int nums : arr){
+            System.out.print(nums + " ");
+        }
+        System.out.println();
+
+    }
+
+}

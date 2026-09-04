46e27cf932dd4dbbe525815dc70f32b3d8af9b73
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-09-04 07:34:19 +0530

    feat: Implemented Better appproch to find the missing element in an array

---
+++ Arrays/MissingNumber/Better.java
@@ -0,0 +1,29 @@
+// Time Complexity = O(n)
+// Space Complexity = O(n)
+
+
+public class Better{
+     public static void FindMissingNumber(int[] arr, int n){
+        int hash[] = new int[n + 2];
+
+        for(int i = 0; i < n; i++){
+            hash[arr[i]] = 1;
+        }
+
+        for(int i = 1; i <= n + 1; i++){
+            if(hash[i] == 0){
+                System.out.println("The missing element is: "+i);
+                break;
+            }
+        }
+    }
+    
+    public static void main(String[] args){
+        int arr[] = {1,2,3,5};
+        int n = arr.length;
+
+        FindMissingNumber(arr, n);
+
+    }
+
+}

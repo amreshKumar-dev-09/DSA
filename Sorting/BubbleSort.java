4f4db6b74e6105fa7e43c354486df8b11e07d061
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-09-05 07:53:08 +0530

    feat: Implemented Bubble Sort Algorithm

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -10,7 +10,7 @@
 
             for(int j = 0; j <= i - 1; j++){
 
-                if(arr[j] > arr[j + 1]){
+                if(arr[j] > arr[j + 1]){ //if(arr[j] > arr[j + 1]) for descending order.
                      int temp = arr[j];
                      arr[j] = arr[j + 1];
                      arr[j + 1] = temp;

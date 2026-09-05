fd027c603da3b93c590129d34d6bc737a6c4ac14
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-09-05 07:52:21 +0530

    feat: Implemented insertion Sort Algorithm

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -5,10 +5,10 @@
 public class InsertionSort {
     public static void sort(int[] arr){
         int n = arr.length;
-        for(int i = 0; i <= n - 1; i++){
+        for(int i = 1; i <= n - 1; i++){
 
             int j = i;
-            while(j > 0 && arr[j - 1] > arr[j]){
+            while(j > 0 && arr[j - 1] > arr[j]){ //while(j > 0 && arr[j - 1] > arr[j]) for descending order.
 
                 int temp = arr[j - 1];
                 arr[j - 1] = arr[j];

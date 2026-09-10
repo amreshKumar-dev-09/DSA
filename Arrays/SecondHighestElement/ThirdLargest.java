e457ff3693c7a3637bc1fac31b3bf36c5cb689b0
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-09-10 09:55:19 +0530

    feat: Implemented Optimal approach to indentify third largest element from an array

---
+++ Arrays/SecondHighestElement/ThirdLargest.java
@@ -0,0 +1,42 @@
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+
+public class ThirdLargest {
+    public static void FindThirdLargest(int[] arr){
+        int n = arr.length;
+        int Largest = arr[0];
+        int SecondLargest = Integer.MIN_VALUE;
+        int ThirdLargest = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i++){
+
+            if(arr[i] > Largest){
+                ThirdLargest = SecondLargest;
+                SecondLargest = Largest;
+                Largest = arr[i];
+            }
+
+            else if(arr[i] < Largest && arr[i] >  SecondLargest){
+                ThirdLargest =  SecondLargest;
+                SecondLargest = arr[i];
+            }
+
+            else if(arr[i] < Largest && arr[i] < SecondLargest && arr[i] > ThirdLargest){
+                ThirdLargest = arr[i];
+            }
+        }
+
+         System.out.println("Third Largest: "+ThirdLargest);
+
+    }
+    
+    public static void main(String[] args){
+        int arr[] = {1,2,4,7,7,5};
+
+        FindThirdLargest(arr);
+    }
+    
+}
+ 
+    
+

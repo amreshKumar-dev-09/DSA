62447488e93e35a9bfa11c8e5fd57234e0a5dd23
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-09-18 09:48:58 +0530

    Implemented Better approach for subarray with sum k

---
+++ Arrays/LongestSubarrayWithSumK/Better.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+
+public class Better{
+   public static void FindLongestSubarray(int[] arr, int k){
+        int n  = arr.length;
+        int maxLength = 0;
+        int sum = 0;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        map.put(0, -1);
+
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+
+            
+            int rem = sum - k;
+
+            if(map.containsKey(rem)){
+                int length = i - map.get((rem));
+                maxLength = Math.max(maxLength, length);
+            }
+
+             if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
+        }
+        System.out.println(maxLength);
+   }
+
+    public static void main(String[] args){
+        int[] arr = {1,2,3,0,0,0,0,4,2,3};
+
+        FindLongestSubarray(arr, 3);
+
+    }
+}

9b9f8f6165a2ea20206d243311854f98f474fc6a
amreshKumar-dev-09 <amreshkumar96179@gmail.com>
2026-09-19 15:38:43 +0530

    feat: Implemented optimal approach for intersection of two sorted arrays

--- Arrays/IntersectionOfTwoArrays/Optimal.java
+++ Arrays/IntersectionOfTwoArrays/Optimal.java
@@ -26,7 +26,7 @@ public class Optimal{
         System.out.println();
 
         }
-
+    
     public static void main(String[] args){
         int[] arr1 = {1, 2, 2, 3, 3, 4, 5, 6};
         int[] arr2 = {2, 3, 3, 5, 6, 7};
